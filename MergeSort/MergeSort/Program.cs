5600845a033f1d543aa5e04862fa4b5aebc0d5e4
David Beylinov <dave93b@gmail.com>
2015-09-13 21:20:10 +0300

    add

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayA = {4,0,6,2,1};
+            int[] arrayB = {9,5,8,7,3};
+
+            int arrayALength = arrayA.Length;
+            int arrayBLength = arrayB.Length;
+
+            BubbleSortForSubArrays(ref arrayA);
+            BubbleSortForSubArrays(ref arrayB);
+
+            int[] resultArray = new int[10];
+            int i = 0, j = 0, k = 0;
+
+            while (i < arrayALength && j < arrayBLength)
+            {
+                if (arrayA[i] < arrayB[j])
+                {
+                    resultArray[k] = arrayA[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    resultArray[k] = arrayB[j];
+                    j++;
+                    k++;
+                }
+            }
+
+            for (int l = i; l < arrayALength; l++)
+            {
+                resultArray[k++] = arrayA[l];
+            }
+
+            for (int l = j; l < arrayBLength; l++)
+            {
+                resultArray[k++] = arrayB[l];
+            }
+
+            foreach (var num in resultArray)
+            {
+                Console.WriteLine(num);
+            }
+            
+            Console.ReadLine();
+        }
+
+        static void BubbleSortForSubArrays(ref int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length-1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MergeSort/MergeSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MergeSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MergeSort")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9aa71f75-72b2-4fed-a3b8-8d4d9e5b63a2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
